823e546d4a5c1bde77978f0c6f150dce7b3994db
namung08 <namung08@gmail.com>
2025-06-27 14:13:16 +0900

    :tada: rocket: 토마토 프로젝트 시작!

---
+++ src/main/java/com/exam/tomatoback/TomatoBackApplication.java
@@ -0,0 +1,13 @@
+package com.exam.tomatoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoBackApplication {
+
+ public static void main(String[] args) {
+  SpringApplication.run(TomatoBackApplication.class, args);
+ }
+
+}
---
+++ src/test/java/com/exam/tomatoback/TomatoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.tomatoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomatoBackApplicationTests {
+
+ @Test
+ void contextLoads() {
+ }
+
+}
