5868bce13d1f93dab3d18e8f8d541cf28bd80d0a
bennett80808 <bennett80808@gmail.com>
2025-06-30 20:50:35 +0900

    마이유저리포지토리 수정정

--- src/main/java/com/exam/tomatoback/myPage/repository/MyUserRepository.java
+++ src/main/java/com/exam/tomatoback/myPage/repository/MyUserRepository.java
@@ -2,6 +2,7 @@ package com.exam.tomatoback.mypage.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.exam.tomatoback.user.model.*;
 
 @Repository
 public interface MyUserRepository extends JpaRepository<User, Long> {
