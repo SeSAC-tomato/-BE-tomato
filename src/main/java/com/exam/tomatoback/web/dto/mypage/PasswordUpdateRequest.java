c7fdc6c3b227ac9ab4360beec51e6c1cd9b74aa4
bennett80808 <bennett80808@gmail.com>
2025-06-30 20:53:41 +0900

    비밀번호 변경 리퀘스트 작성성

---
+++ src/main/java/com/exam/tomatoback/web/dto/mypage/PasswordUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.exam.tomatoback.web.dto.mypage;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PasswordUpdateRequest {
+    String currentPassword;
+    String newPassword;
+    String confirmPassword;
+}
