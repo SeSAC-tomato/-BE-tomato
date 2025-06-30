1d4e8a6ce4441ce140a7313f467da42445334a45
namung08 <namung08@gmail.com>
2025-06-30 10:18:12 +0900

    :sparkles: feat: 사용자 모델 추가 - User 도메인 및 Role, Provider 열거형 클래스 생성

---
+++ src/main/java/com/exam/tomatoback/user/model/Provider.java
@@ -0,0 +1,8 @@
+package com.exam.tomatoback.user.model;
+
+import lombok.Getter;
+
+@Getter
+public enum Provider {
+    LOCAL;
+}
---
+++ src/main/java/com/exam/tomatoback/user/model/Role.java
@@ -0,0 +1,11 @@
+package com.exam.tomatoback.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+    ADMIN("ROLE_ADMIN"),USER("ROLE_USER");
+    private final String rolename;
+}
