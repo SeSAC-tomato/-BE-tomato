5ba462474a9d3e7f8efece86407813cc47973f9b
namung08 <namung08@gmail.com>
2025-06-30 13:25:57 +0900

    :sparkles: feat: 사용자 등록 메서드 개선 - 이메일 및 닉네임 중복 확인 추가, 비밀번호 암호화 및 주소 정보 설정 기능 구현

--- src/main/java/com/exam/tomatoback/auth/service/AuthServiceImpl.java
+++ src/main/java/com/exam/tomatoback/auth/service/AuthServiceImpl.java
@@ -1,23 +1,58 @@
 package com.exam.tomatoback.auth.service;
 
-import com.exam.tomatoback.auth.repository.UserRepository;
 import com.exam.tomatoback.infrastructure.exception.TomatoException;
 import com.exam.tomatoback.infrastructure.exception.TomatoExceptionCode;
+import com.exam.tomatoback.user.model.Address;
+import com.exam.tomatoback.user.model.Provider;
+import com.exam.tomatoback.user.model.Role;
+import com.exam.tomatoback.user.model.User;
+import com.exam.tomatoback.user.repository.UserRepository;
 import com.exam.tomatoback.web.dto.auth.request.RegisterRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
 
     @Override
     public void register(RegisterRequest registerRequest) {
-        boolean present = userRepository.findByEmail(registerRequest.getEmail()).isPresent();
-        // 사용자가 없을 경우
-        if (present) {
-            throw new TomatoException(TomatoExceptionCode.CONFLICT_USER);
+        // 이메일 중복 여부 확인
+        if(userRepository.existsByEmail(registerRequest.getEmail())) {
+            throw new TomatoException(TomatoExceptionCode.DUPLICATE_USER);
         }
+        // 닉네임 중복 여부 확인
+        if(userRepository.existsByNickname(registerRequest.getNickname())) {
+            throw new TomatoException(TomatoExceptionCode.DUPLICATE_USER);
+        }
+        // 비밀번호 검증
+        if(!registerRequest.getPassword().equals(registerRequest.getPasswordConfirm())) {
+            throw new TomatoException(TomatoExceptionCode.PASSWORD_MISMATCH);
+        }
+        // 비밀번호 암호화
+        registerRequest.setPassword(encoder.encode(registerRequest.getPassword()));
+
+        // 신규 사용자 정보 설정
+        User newUser = User.builder()
+                .email(registerRequest.getEmail())
+                .nickname(registerRequest.getNickname())
+                .password(registerRequest.getPassword())
+                .provider(Provider.LOCAL)
+                .role(Role.USER)
+                .build();
+
+        // 주소 정보 설정
+        Address address = Address.builder()
+                .user(newUser)
+                .address(registerRequest.getAddress())
+                .build();
+
+        newUser.setAddress(address);
+
+        // 사용자 저장
+        userRepository.save(newUser);
     }
 }
