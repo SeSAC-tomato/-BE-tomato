ca3956f04900fd83f4189262a961c67657270e3a
namung08 <namung08@gmail.com>
2025-06-30 17:03:49 +0900

    :sparkles: feat: cors 설정

--- src/main/java/com/exam/tomatoback/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/exam/tomatoback/infrastructure/config/SecurityConfig.java
@@ -15,12 +15,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Component
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtFilter jwtFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -30,6 +36,19 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
+        config.setExposedHeaders(Collections.singletonList("Authorization"));
+        config.setMaxAge(3600L);
+
+        return request -> config;
     }
 
     @Bean
@@ -45,6 +64,9 @@ public class SecurityConfig {
             .requestMatchers(Constants.PUBLIC_PATH).permitAll()
             .anyRequest().authenticated()
         );
+
+        // cors 설정을 적용하기 위한 설정
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource));
 
         return http.build();
     }
