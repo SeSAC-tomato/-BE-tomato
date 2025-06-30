9704326319516628936c67f64a02937a5592c387
bennett80808 <bennett80808@gmail.com>
2025-06-30 20:54:42 +0900

    유저 정보 수정 리퀘스트 작성성

---
+++ src/main/java/com/exam/tomatoback/web/dto/mypage/UserUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.exam.tomatoback.web.dto.mypage;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserUpdateRequest {
+    private String nickname;
+    private String address;
+}
