9423bd2bd596af51c9debe6a61b5320013bc9953
bennett80808 <bennett80808@gmail.com>
2025-06-30 20:51:23 +0900

    마이유저서비스 수정

--- src/main/java/com/exam/tomatoback/myPage/service/MyUserService.java
+++ src/main/java/com/exam/tomatoback/myPage/service/MyUserService.java
@@ -1,32 +1,11 @@
 package com.exam.tomatoback.mypage.service;
 
-import com.exam.tomatoback.mypage.repository.MyUserRepository;
-import com.exam.tomatoback.web.dto.myPageDto.UserResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.exam.tomatoback.web.dto.mypage.*;
 
-import java.util.NoSuchElementException;
+public interface MyUserService {
+    UserResponse getUserById(Long id);
 
-@Service
-@RequiredArgsConstructor
-@Transactional
-public class MyUserService {
+    UserUpdatedResponse updateUserById(Long userId, UserUpdateRequest userUpdateRequest);
 
-    private final MyUserRepository userRepository;
-
-    @Transactional(readOnly = true)
-    public UserResponse getUserById(Long userId) {
-        User user = userRepository.findById(userId).orElseThrow(()-> new NoSuchElementException("유저가 존재하지 않습니다."));
-
-        String address = null;
-        if(user.getAddress() != null) {
-            address = user.getAddress().getAddress();
-        }
-        return new UserResponse(
-                user.getNickname(),
-                user.getEmail(),
-                address
-        );
-    }
+    PasswordUpdatedResponse updatePasswordById(Long userId, PasswordUpdateRequest request);
 }
