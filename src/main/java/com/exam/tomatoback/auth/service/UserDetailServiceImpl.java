bc1da30f09b3affe1f64f5b117b3653d05f34518
namung08 <namung08@gmail.com>
2025-06-30 10:16:54 +0900

    :sparkles: feat: 사용자 세부 정보 서비스 구현 - 사용자 인증을 위한 UserDetailsService 추가

---
+++ src/main/java/com/exam/tomatoback/auth/service/UserDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package com.exam.tomatoback.auth.service;
+
+import com.exam.tomatoback.infrastructure.exception.TomatoException;
+import com.exam.tomatoback.infrastructure.exception.TomatoExceptionCode;
+import com.exam.tomatoback.infrastructure.util.JwtUtil;
+import com.exam.tomatoback.user.model.User;
+import com.exam.tomatoback.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new TomatoException(TomatoExceptionCode.USER_NOT_FOUND));
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(Collections.singletonList(new SimpleGrantedAuthority(user.getRole().getRolename())))
+                .build();
+    }
+}
