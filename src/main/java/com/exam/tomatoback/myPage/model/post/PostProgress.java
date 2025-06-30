0f38946c331070cd8c9406b42b5f50357dadaf35
bennett80808 <bennett80808@gmail.com>
2025-06-30 15:32:12 +0900

    PostProgress 엔티티 생성성

---
+++ src/main/java/com/exam/tomatoback/myPage/model/post/PostProgress.java
@@ -0,0 +1,34 @@
+package com.exam.tomatoback.mypage.model.post;
+
+import com.exam.tomatoback.mypage.model.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "post_progress")
+public class PostProgress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id")
+    private User buyer;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+
+
+}
