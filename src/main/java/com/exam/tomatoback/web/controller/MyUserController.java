618a42b2830e193e65faf0c342b82009a66682fb
bennett80808 <bennett80808@gmail.com>
2025-06-30 20:52:43 +0900

    마이유저컨트롤러 작성성

--- src/main/java/com/exam/tomatoback/web/controller/MyUserController.java
+++ src/main/java/com/exam/tomatoback/web/controller/MyUserController.java
@@ -2,13 +2,10 @@ package com.exam.tomatoback.web.controller;
 
 import com.exam.tomatoback.mypage.service.MyUserService;
 import com.exam.tomatoback.web.dto.common.CommonResponse;
-import com.exam.tomatoback.web.dto.myPageDto.UserResponse;
+import com.exam.tomatoback.web.dto.mypage.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/user")
@@ -21,9 +18,25 @@ public class MyUserController {
     public ResponseEntity<CommonResponse<UserResponse>> getUserById(
             @PathVariable Long userId
     ){
-        UserResponse user = userService.getUserById(userId);
-        return ResponseEntity.ok(CommonResponse.success(user));
+        UserResponse response = userService.getUserById(userId);
+        return ResponseEntity.ok(CommonResponse.success(response));
     }
 
+    @PutMapping("/{userId}/profile")
+    public ResponseEntity<CommonResponse<UserUpdatedResponse>> updateUserById(
+            @PathVariable Long userId,
+            @RequestBody UserUpdateRequest request
+    ){
+        UserUpdatedResponse response = userService.updateUserById(userId, request);
+        return ResponseEntity.ok(CommonResponse.success(response));
+    }
 
+    @PutMapping("/{userId}/password")
+    public ResponseEntity<CommonResponse<PasswordUpdatedResponse>> updatePasswordById(
+            @PathVariable Long userId,
+            @RequestBody PasswordUpdateRequest request
+            ){
+        PasswordUpdatedResponse response = userService.updatePasswordById(userId, request);
+        return ResponseEntity.ok(CommonResponse.success(response));
+    }
 }
