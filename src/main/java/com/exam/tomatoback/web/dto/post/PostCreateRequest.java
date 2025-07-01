3e4688e7f380165b64d1cbbf00214a911d33ea17
ellanelee <ljedoc@naver.com>
2025-07-02 07:32:02 +0900

    :wrench: feat: 이미지 저장을 위한 경로추가

--- src/main/java/com/exam/tomatoback/web/dto/post/PostCreateRequest.java
+++ src/main/java/com/exam/tomatoback/web/dto/post/PostCreateRequest.java
@@ -3,10 +3,7 @@ package com.exam.tomatoback.web.dto.post;
 import com.exam.tomatoback.post.model.Post;
 import com.exam.tomatoback.post.model.PostStatus;
 import com.exam.tomatoback.post.model.ProductCategory;
-import com.exam.tomatoback.user.model.User;
 import lombok.*;
-
-import java.util.List;
 
 @Getter
 @Setter
