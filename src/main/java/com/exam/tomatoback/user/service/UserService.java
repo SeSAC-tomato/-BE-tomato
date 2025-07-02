b5bc1673a33b3dbd60650ea6fcee31ac965a4c5c
bennett80808 <bennett80808@gmail.com>
2025-07-02 21:45:04 +0900

    ✨feat 마이페이지 유저 관련된 서비스 추가가

--- src/main/java/com/exam/tomatoback/user/service/UserService.java
+++ src/main/java/com/exam/tomatoback/user/service/UserService.java
@@ -1,6 +1,11 @@
 package com.exam.tomatoback.user.service;
 
 import com.exam.tomatoback.user.model.User;
+import com.exam.tomatoback.web.dto.mypage.request.PasswordUpdateRequest;
+import com.exam.tomatoback.web.dto.mypage.request.UserUpdateRequest;
+import com.exam.tomatoback.web.dto.mypage.response.PasswordUpdatedResponse;
+import com.exam.tomatoback.web.dto.mypage.response.UserResponse;
+import com.exam.tomatoback.web.dto.mypage.response.UserUpdatedResponse;
 
 import java.util.Optional;
 
@@ -12,4 +17,13 @@ public interface UserService {
     boolean existsByNickname(String nickname);
 
     Optional<User> getOptionalUser(String email);
+
+    // userId 로 사용자 정보 조회
+    UserResponse getUserById(Long id);
+
+    // userId 로 사용자 정보 수정
+    UserUpdatedResponse updateUserById(Long userId, UserUpdateRequest request);
+
+    // userId로 비밀번호 수정
+    PasswordUpdatedResponse updatePasswordById(Long userId, PasswordUpdateRequest request);
 }
