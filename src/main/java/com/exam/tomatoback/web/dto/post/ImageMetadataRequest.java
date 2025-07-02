4d6fd554010fad3e4255913b37be10810157dddb
ellanelee <ljedoc@naver.com>
2025-07-02 11:34:56 +0900

    :sparkles: feat: 이미지 처리를 위한 DTO추가(이미지 메타데이터, 이미지를 포함한 Post)

---
+++ src/main/java/com/exam/tomatoback/web/dto/post/ImageMetadataRequest.java
@@ -0,0 +1,15 @@
+package com.exam.tomatoback.web.dto.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageMetadataRequest {
+      private String originalFileName;
+      private Boolean isMain;
+}
