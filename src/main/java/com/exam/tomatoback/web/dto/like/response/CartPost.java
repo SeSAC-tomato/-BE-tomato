0fe42163b42126332db47074fd8fd289f612bb94
bennett80808 <bennett80808@gmail.com>
2025-07-02 21:47:06 +0900

    ✨feat 관심목록 관련 DTO 추가가

---
+++ src/main/java/com/exam/tomatoback/web/dto/like/response/CartPost.java
@@ -0,0 +1,22 @@
+package com.exam.tomatoback.web.dto.like.response;
+
+import com.exam.tomatoback.post.model.PostCategory;
+import com.exam.tomatoback.post.model.PostStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartPost {
+
+        private String title;
+        private Integer price;
+        private String img;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+        private PostStatus status;
+        private PostCategory category;
+}
