568793318ebc5965a5976c557b998490fc1273cc
ellanelee <ljedoc@naver.com>
2025-07-02 21:05:06 +0900

    :sparkles: feat: Like기능 추가

---
+++ src/main/java/com/exam/tomatoback/post/repository/LikeRepository.java
@@ -0,0 +1,14 @@
+package com.exam.tomatoback.post.repository;
+
+import com.exam.tomatoback.post.model.Like;
+import com.exam.tomatoback.post.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface LikeRepository  extends JpaRepository<Like, Long> {
+
+}
