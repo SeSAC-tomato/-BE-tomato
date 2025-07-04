b8a24db1d1100bb3d82c3eec26ab767988e945a5
bennett80808 <bennett80808@gmail.com>
2025-07-04 10:30:05 +0900

    :adhesive_bandage: refactor enum ProductCategory로 수정

--- src/main/java/com/exam/tomatoback/post/model/Post.java
+++ src/main/java/com/exam/tomatoback/post/model/Post.java
@@ -39,7 +39,7 @@ public class Post {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private PostCategory postCategory;
+    private ProductCategory productCategory;
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
