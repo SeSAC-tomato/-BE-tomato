1d9e4f26469cec1c87c0d3f2d5fa0be542ffee72
namung08 <namung08@gmail.com>
2025-07-04 17:15:45 +0900

    :sparkles: feat: 사용자 인증을 위한 dto

--- src/main/java/com/exam/tomatoback/web/dto/auth/request/VerifyRequest.java
+++ src/main/java/com/exam/tomatoback/web/dto/auth/request/VerifyRequest.java
@@ -1,4 +1,19 @@
 package com.exam.tomatoback.web.dto.auth.request;
 
-public record VerifyRequest() {
+import com.exam.tomatoback.auth.model.VerityType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+@Builder
+public record VerifyRequest(
+    @Email(message = "이메일 형식이 아닙니다.")
+    @NotBlank(message = "이메일은 필수 값입니다.")
+    String email,
+    @NotNull(message = "인증 토큰은 필수입니다.")
+    String token,
+    @NotNull(message = "인증 타입은 필수입니다.")
+    VerityType type
+) {
 }
