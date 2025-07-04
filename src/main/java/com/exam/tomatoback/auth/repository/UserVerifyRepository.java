1384eb1c341e7006b84cdde6592e1a7f59b7e05a
namung08 <namung08@gmail.com>
2025-07-04 17:17:29 +0900

    :sparkles: feat: 인증 정보 조회 기능 추가

--- src/main/java/com/exam/tomatoback/auth/repository/UserVerifyRepository.java
+++ src/main/java/com/exam/tomatoback/auth/repository/UserVerifyRepository.java
@@ -3,5 +3,8 @@ package com.exam.tomatoback.auth.repository;
 import com.exam.tomatoback.auth.model.UserVerify;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserVerifyRepository extends JpaRepository<UserVerify, Long> {
+  Optional<UserVerify> findByToken(String token);
 }
