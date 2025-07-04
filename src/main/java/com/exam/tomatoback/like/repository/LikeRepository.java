273d65b7775b6d18afab22eb8276f529592384ee
bennett80808 <bennett80808@gmail.com>
2025-07-04 10:14:42 +0900

    :sparkles: feat LikeRepository 추가

---
+++ src/main/java/com/exam/tomatoback/like/repository/LikeRepository.java
@@ -0,0 +1,13 @@
+package com.exam.tomatoback.like.repository;
+
+import com.exam.tomatoback.like.model.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByUserIdAndPostId(Long userId, Long postId);
+}
+
