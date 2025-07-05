2c2342e715b01cdefeffb12b4e57f68f8bb4b753
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2025-07-05 12:51:42 +0900

    :sparkles: feat: 챗 관련 Controller 추가

---
+++ src/main/java/com/exam/tomatoback/web/controller/chat/ChatApiController.java
@@ -0,0 +1,69 @@
+package com.exam.tomatoback.web.controller.chat;
+
+import com.exam.tomatoback.chat.service.ChatApiService;
+import com.exam.tomatoback.web.dto.chat.api.ChatListPageRequest;
+import com.exam.tomatoback.web.dto.chat.api.ChatPageRequest;
+import com.exam.tomatoback.web.dto.chat.api.ChatRoomRequest;
+import com.exam.tomatoback.web.dto.chat.api.ChatRoomResponse;
+import com.exam.tomatoback.web.dto.common.CommonResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/chat")
+public class ChatApiController {
+    private final ChatApiService chatApiService;
+
+
+    // 채팅 목록 불러오기
+    @GetMapping
+    public ResponseEntity<?> getChatList(@Valid ChatListPageRequest chatListPageRequest) {
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(chatApiService.getChatList(chatListPageRequest)));
+    }
+
+
+    // 채팅룸에 있는 채팅 불러오기
+    @GetMapping("/{roomId}")
+    public ResponseEntity<?> getChat(@Valid ChatPageRequest chatPageRequest, @PathVariable long roomId) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(chatApiService.getChats(chatPageRequest, roomId)));
+    }
+
+    // 신규 채팅 룸 만들기
+    @GetMapping("/room")
+    public ResponseEntity<?> getChatRoom(ChatRoomRequest chatRoomRequest) {
+        long chatRoomIdOrCreateRoomId = chatApiService.getChatRoomIdOrCreateRoomId(chatRoomRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(new ChatRoomResponse(chatRoomIdOrCreateRoomId)));
+    }
+
+    // 마지막 읽은 챗
+    @GetMapping("/room/{roomId}/chat/{chatId}")
+    public ResponseEntity<?> setLastRead(@PathVariable long roomId, @PathVariable long chatId) {
+        chatApiService.setLastRead(roomId,chatId);
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(null));
+    }
+
+    // 예약 진행 상황 확인
+    @GetMapping("/room/{roomId}")
+    public ResponseEntity<?> getRoomInfo(@PathVariable long roomId) {
+        return  ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(chatApiService.getRoomInfo(roomId)));
+    }
+
+    // 판매중인 상품 확인
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<?> getUserSellingList(@PathVariable long userId) {
+        return  ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(chatApiService.getUserSellingList(userId)));
+    }
+
+
+}
