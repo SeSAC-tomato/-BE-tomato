cb4f935e064db8a7525a480224d54b5e73b368c9
bennett80808 <bennett80808@gmail.com>
2025-07-07 17:30:18 +0900

    :adhesive_bandage: refactor LikeSort 값 올바르게 수정

--- src/main/java/com/exam/tomatoback/web/dto/like/request/LikeSort.java
+++ src/main/java/com/exam/tomatoback/web/dto/like/request/LikeSort.java
@@ -2,7 +2,7 @@ package com.exam.tomatoback.web.dto.like.request;
 
 public enum LikeSort {
     LIKE_CREATED_AT("LIKE_CREATED_AT"), // LIKE 생성 시각
-    POST_UPDATED_AT("post.createdAt"), // 게시글 생성 시각. 이건 post.createdAt값을 사용함
+    POST_UPDATED_AT("post.updatedAt"), // 게시글 생성 시각. 이건 post.updatedAt값을 사용함
     PRICE("PRICE"), // 게시글 가격
     POPULARITY("POPULARITY");   // 게시글의 총 좋아요 수
 
