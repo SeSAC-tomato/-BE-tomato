2c56d80eb675fe5f782f06d37d75ad0b8cd4d681
bennett80808 <bennett80808@gmail.com>
2025-07-08 21:56:13 +0900

    :zap: perf dto변환과정 서비스로 옮김

--- src/main/java/com/exam/tomatoback/like/service/LikeService.java
+++ src/main/java/com/exam/tomatoback/like/service/LikeService.java
@@ -1,12 +1,13 @@
 package com.exam.tomatoback.like.service;
 
 import com.exam.tomatoback.web.dto.like.request.CartGetRequest;
+import com.exam.tomatoback.web.dto.like.request.LikeSort;
 import com.exam.tomatoback.web.dto.like.response.CartGetResponse;
 
 public interface LikeService {
 
     // userId로 해당 사용자 관심목록 가져오기
-    CartGetResponse getCartByUserId(Long userId, CartGetRequest request);
+    CartGetResponse getCartByUserId(Long userId, Integer currentPage, Integer size, LikeSort likeSortStr);
 
     void deleteLike(Long userId, Long postId);
 }
